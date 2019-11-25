96ba528985784655a6c6a495e68048e8c8bc6d22
rodielg <rodi_elg@hotmail.com>
2019-11-25 19:37:16 -0300

    Update Funciones.m

--- Funciones.m
+++ Funciones.m
@@ -1,12 +1,12 @@
 function [funcion] = Funciones()
 %Funcion para guardar todas las funciones que hicimos en un struct
     funcion.sineSweep=@SineSweep;
-    funcion.ruidoRosa=@RuidoRosa;
+    funcion.ruidoRosa=@ruidoRosa;
     funcion.filtroInverso=@filtroInverso;
     funcion.filtroInvertido=@filtroInvertido;
     funcion.filtroNormaIEC=@filtroNormaIEC;
     funcion.RI=@RI;
-    funcion.sintetizacionRI=@sintetizacionRI;
+    funcion.sintetizacionRI=@sintetizacionRI;]
     funcion.rtaAlImpulso=@rtaAlImpulso;
     funcion.convLog=@convLog
     
@@ -14,7 +14,9 @@ end
 
 function x = ruidoRosa(duracion,fs);
 %Duracion en segundos. fs en Hertz
-Nx = duracion*fs;
+%Genera un Ruido Rosa sintetizado
+%%
+Nx = duracion*fs; %Obtengo cantidad de muestras del audio
 B = [0.049922035 -0.095993537 0.050612699 -0.004408786];
 A = [1 -2.494956002 2.017265875 -0.522189400];
 nT60 = round(log(1000)/(1-max(abs(roots(A)))));
@@ -23,6 +25,11 @@ x = filter(B,A,v);
 x = x(nT60+1:end);
 end
 function k = filtroInverso(T,Fs,f1,f2);
+%% Obtencion del Filtro Inverso del SineSweep
+%Inputs:
+% T = Tiempo en [s]
+% Fs = Frecuencia de muestreo en [Hz]
+% f1,f2 = Frecuencias limite inferior y superior del SineSweep [Hz]
 x = filtroInvertido(T,Fs,f1,f2);
 y = sineSweep(T,Fs,f1,f2);
 w1 = 2*pi*f1;
@@ -33,19 +40,19 @@ w = (K/L)*exp(y/L);
 m = w1./(2*pi*w);
 k = m.*x;
 end
-function [h] = RI(x,y)
-%UNTITLED7 Summary of this function goes here
-%   Detailed explanation goes here
-Y=fft(y);
-X = fft(fliplr(x));
-H=Y.*X;
+function [h] = RI(k,y)
+%% Obtencion de la respuesta al impulso a traves de la grabacion y el filtro
+%inverso
+K=fft(k);
+H=Y.*K;
 h=ifft(H);
 end
 function [suma] = sintetizacionRI(f,T60,A,t)
-%UNTITLED5 Summary of this function goes here
-%   Detailed explanation goes here
-y=A*(e^pi1*t)*cos(2*pi*f*t);
+%UN
+t=t:-1/44100:0;
+t=t(1:end-1);
 pi1=(-log(10^(-3)))/T60;
+y=A*(exp(pi1*t)).*cos(2*pi*f*t);
 suma=sum(y);
 end
 function [audioFilt] = filtroNormaIEC(audio)
@@ -60,8 +67,8 @@ audioFilt=filtfilt(filter,audio);
 end
 function x= filtroInvertido(T,Fs,f1,f2)
 N = T*Fs;
-y = T:(-1)/Fs:0;
-%y = linspace(0,T,N);
+%y = T:(-1)/Fs:0;
+y = linspace(0,T,N);
 w1 = 2*pi*f1;
 w2 = 2*pi*f2;
 K = (T*w1)/(log(w2/w1));
